e0851533947166f15c4e22268a02e55334d047d4
Qilan Lin <78576883+qilanlin@users.noreply.github.com>
2022-11-19 14:44:26 +0000

    Create upload file.py
    Realize the part of file uploading function

---
+++ upload file.py
@@ -0,0 +1,21 @@
+import arweave
+from arweave.arweave_lib import Transaction
+
+
+wallet_file_path = "/some/folder/on/your/system"
+wallet = arweave.Wallet(wallet_file_path)
+
+
+balance =  wallet.balance
+print('Balance='+balance)
+
+with open('myfile.pdf', 'r') as mypdf:
+    pdf_string_data = mypdf.read()
+
+    transaction = Transaction(wallet, data=pdf_string_data)
+    transaction.sign()
+    transaction.send()
+
+last_transaction = wallet.get_last_transaction_id()
+balance =  wallet.balance
+print('Balance='+balance)
